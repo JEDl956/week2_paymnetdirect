632a265dbec2cc513811faccab885d0896fa8ca0
Jonathan E De Leon <jdele218@stu.southtexascollege.edu>
2026-02-06 23:59:27 -0600

    added card

---
+++ PaymentDirect/src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="card">
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+.card {
+  width: min(600px, 100%);
+  background: #1e1e1e; /* card dark gray */
+  padding: 18px;
+  border-radius: 16px;
+  box-shadow: 0 10px 24px rgba(0, 0, 0, 0.6);
+}
+</style>
